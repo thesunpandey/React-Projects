ac56c143bef2c98a326fcbf73bbceac49366dca1
Suraj Pandey <pandeyjisuraj15@gmail.com>
2024-08-21 15:07:40 +0530

    Created Chat Application Using React

--- Project_14_Chat_Application/chat-application/src/App.jsx
+++ Project_14_Chat_Application/chat-application/src/App.jsx
@@ -1,13 +1,22 @@
-/* eslint-disable */
+import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css'
+const projectID = '705487f3-46a4-4e0b-930f-64255c83109b';
 
-import React from 'react'
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
 
-function App() {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
 
-export default App
+export default App;
