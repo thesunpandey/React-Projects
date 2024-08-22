b8c48bff4cd205547542e436288d07aa8be42d19
Suraj Pandey <pandeyjisuraj15@gmail.com>
2024-08-22 16:08:55 +0530

    Created Todo List App

---
+++ Project_17_Todo_List_App/todo-list-app/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable */
+import React from "react";
+
+const date = new Date();
+
+function Footer(){
+  return(
+    <footer>
+      <p> Copyright {date.getFullYear()}. </p>
+    </footer>
+  )
+}
+
+export default Footer;
---
+++ Project_17_Todo_List_App/todo-list-app/src/components/Header.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable */
+
+import React from "react";
+import HighlightIcon from "@mui/icons-material/Highlight";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <HighlightIcon />
+        ToDoList
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
