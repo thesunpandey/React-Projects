15e0067594cfa08f7e2922af6dfa0f737378c9f9
Suraj Pandey <pandeyjisuraj15@gmail.com>
2024-08-23 21:09:54 +0530

    Created Resume Website

---
+++ Project_19_Resume_Website/resume-website/src/main.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+// Create a root
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the App component
+root.render(<App />);
