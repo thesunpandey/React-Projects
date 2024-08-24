900b7357543ef8b651bd2784de4ef7d532cfcdfe
Suraj Pandey <pandeyjisuraj15@gmail.com>
2024-08-24 20:55:34 +0530

    Created Random Person App

---
+++ Project_20_Random_Person_App/random-person-app/src/components/SignInAndSignUp.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable */
+
+import React from 'react'
+
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+
+const SignInAndSignUp = () => (
+  <div>
+    <SignIn />
+    <SignUp />
+  </div>
+);
+
+export default SignInAndSignUp;
