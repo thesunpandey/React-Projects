9b10f28f621639e0ebee43a8541e8ffcbb045157
Suraj Pandey <pandeyjisuraj15@gmail.com>
2024-08-26 20:58:05 +0530

    Created Color Code Generator

---
+++ Project_25_Color_Code_Generator/color-code-generator/src/App.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable */
+import React from 'react'
+
+import Dashboard from "./components/Dashboard.jsx";
+function App() {
+  return (
+    <>
+      <Dashboard />
+    </>
+  );
+}
+
+export default App;
---
+++ Project_25_Color_Code_Generator/color-code-generator/src/components/Dashboard.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import Values from "values.js";
+import SingleColor from "../components/SingleColor";
+import "./Dashboard.css";
+function Dashboard() {
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState(new Values("#2c2e3b").all(10));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors);
+    } catch (error) {
+      setError(error);
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="#2e2c3b"
+            className={`${error ? "error" : null}`}
+          />
+          <button className="btn" type="submit">
+            submit
+          </button>
+        </form>
+      </section>
+      <section className="colors">
+        {list.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          );
+        })}
+      </section>
+    </>
+  );
+}
+export default Dashboard;
